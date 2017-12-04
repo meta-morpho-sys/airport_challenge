460d67f9c6a18b9675d78e726985ef147dbf248e
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-12-04 23:09:52 +0000

    Refactored the feature test

--- spec/air_traffic_control_feature_spec.rb
+++ spec/air_traffic_control_feature_spec.rb
@@ -2,32 +2,19 @@ require 'airport'
 require 'plane'
 require 'weather'
 
-def maximum_capacity_filled
-  airport.land(plane_1, weather)
-  airport.land(plane_2, weather)
-  airport.land(plane_3, weather)
-  airport.land(plane_4, weather)
-  airport.land(plane_5, weather)
-end
-
 describe 'Air traffic controller' do
   let(:airport) { Airport.new 5 }
-  let(:plane_1) { Plane.new true }
-  let(:plane_2) { Plane.new true }
-  let(:plane_3) { Plane.new true }
-  let(:plane_4) { Plane.new true }
-  let(:plane_5) { Plane.new true }
 
   context 'when airport is full' do
     let(:weather) { Weather.new false }
+    before(:each) do
+      5.times { airport.land(Plane.new, weather) }
+    end
 
     it 'prevents a plane from landing' do
-      maximum_capacity_filled
       new_plane = Plane.new true
       expect { airport.land(new_plane, weather) }.to raise_exception Airport::NO_SPACE_MSG
-
-      landed_planes = [plane_1, plane_2, plane_3, plane_4, plane_5]
-      expect(airport.planes).to eq landed_planes
+      expect(airport.planes.size).to eq 5
       expect(new_plane).not_to be_landed
     end
   end
@@ -36,9 +23,10 @@ describe 'Air traffic controller' do
     let(:weather) { Weather.new false }
 
     it 'lands a plane ' do
-      airport.land(plane_1, weather)
-      expect(airport.planes).to eq [plane_1]
-      expect(plane_1).to be_landed
+      plane = Plane.new
+      airport.land(plane, weather)
+      expect(airport.planes).to eq [plane]
+      expect(plane).to be_landed
     end
   end
 
